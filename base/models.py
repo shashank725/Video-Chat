028f4cb3b23dd8ee878e4f0434d409b77ec1d5cd
shashank725 <shashank.nov725@gmail.com>
2022-09-05 21:03:18 +0530

    [ADD] models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class RoomMember(models.Model):
+    # name = models.ForeignKey(User, on_delete=models.CASCAD)
+    name = models.CharField(max_length=200)
+    uid = models.CharField(max_length=1000)
+    room_name = models.CharField(max_length=200)
+    insession = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
